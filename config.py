4f823fb01ffe434779f8fac75f843f90b5e2f642
Ahmed Alaa <ahmedalaa.10@hotmail.com>
2021-04-24 04:29:02 +0200

    add: GUI config

---
+++ config.py
@@ -0,0 +1,28 @@
+fonts_paths = ['fonts\\Ubuntu-Light.ttf', 'fonts\\Ubuntu-Regular.ttf', 'fonts\\UbuntuMono-Regular.ttf']
+primary_font = 'Ubuntu'
+secondary_font = 'Ubuntu Mono'
+app_background = 'background-color: #363636;'
+
+label_style =   """
+                    color: white; 
+                    font-size: 15px
+                """
+
+input_style =   """
+                    color: white; 
+                    border: none; 
+                    background-color: #212121;
+                    font-size: 11px
+                """
+
+output_style =  """
+                    color: green; 
+                    border: none; 
+                    background-color: black;
+                """
+
+btn_style =     """
+                    background-color: #212121; 
+                    width: 100px; 
+                    color: white;
+                """
