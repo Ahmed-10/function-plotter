d085f643e18f6936354af85826b0965b29e46ba2
Ahmed Alaa <ahmedalaa.10@hotmail.com>
2021-04-24 04:30:13 +0200

    update main.py

--- main.py
+++ main.py
@@ -1,28 +1,8 @@
-import matplotlib.pyplot as plt
-import numpy as np
-from interpreter import Interpreter
-from math_parser import Parser
-from lexer import Lexer
+import sys
+from PySide2.QtWidgets import QApplication
+from view import WinView
 
-while True:
-       text = input("calc >  ")
-       lexer = Lexer(text)
-       tokens = lexer.generate_tokens()
-       # print(list(tokens))
-       parser = Parser(tokens)
-       tree = parser.parse()
-       # print(tree)
-       if not tree: continue
-       range = np.arange(0.0, 1.0, 0.1)
-       interpreter = Interpreter(0.0, 1.0, 0.1)
-       value = interpreter.visit(tree)
-       print(value)
-       fig, ax = plt.subplots()
-       ax.plot(range, value)
+app = QApplication([])
 
-       ax.set(xlabel='time (s)', ylabel='voltage (mV)',
-              title='About as simple as it gets, folks')
-       ax.grid()
-
-       fig.savefig("test.png")
-       plt.show()
+widget = WinView()
+sys.exit(app.exec_())
